1be64901f80ad116be70aebf6fb935f1c0cef919
Patrik Reizinger <prmedia17@gmail.com>
2023-04-07 20:24:02 +0200

    dummy test, lti.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,2 @@
 import pytest
-from pl_bolts.datamodules import CIFAR10DataModule
-from pytorch_lightning.core.datamodule import LightningDataModule
 
-from vae_sam.utils import CIFAR10_DIR
-
-
-@pytest.fixture
-def cifar10dm() -> LightningDataModule:
-    return CIFAR10DataModule(data_dir=CIFAR10_DIR)
