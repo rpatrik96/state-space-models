b949e4d1136177b3833af37827f50dcb6955b80b
Patrik Reizinger <prmedia17@gmail.com>
2023-04-08 09:05:06 +0200

    test_spring_mass

--- tests/test_lti.py
+++ tests/test_lti.py
@@ -1,2 +1,10 @@
 def test_dummy():
     assert True == True
+
+
+from state_space_models.lti import SpringMassDamper
+
+
+def test_spring_mass():
+    s = SpringMassDamper.from_params()
+    m = s.controllability
