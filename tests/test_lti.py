5ea9417a1e6c051167871164ff749899832cd157
Patrik Reizinger <prmedia17@gmail.com>
2023-04-19 10:03:17 +0200

    lti_AB conftest

--- tests/test_lti.py
+++ tests/test_lti.py
@@ -2,11 +2,18 @@ from state_space_models.lti import SpringMassDamper, LTISystem
 import numpy as np
 
 
-def test_lti_AB():
+def test_lti_AB(lti_AB):
+    lti_AB
+
+
+def test_lti_simulate(lti_AB):
+    num_step = 50
     dim = 2
-    A = np.random.randn(dim, dim)
-    B = np.random.randn(dim, dim)
-    lti = LTISystem(A=A, B=B)
+    U = np.random.randn(num_step, dim)
+    t, y, x = lti_AB.simulate(U, dt=0.1)
+
+    assert x.shape == (num_step, dim)
+    assert y.shape == (num_step, lti_AB.C.shape[0])
 
 
 def test_spring_mass():
