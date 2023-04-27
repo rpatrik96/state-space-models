a932f92f3c4c78f8e3901784f07875517d4cd468
Patrik Reizinger <prmedia17@gmail.com>
2023-04-27 17:03:14 +0200

    LTI dataset for torch

---
+++ state_space_models/dataset.py
@@ -0,0 +1,27 @@
+from torch.utils.data import Dataset
+import torch
+from .lti import LTISystem
+
+
+class LTIDataset(Dataset):
+    """
+    A pytorch dataset that takes an LTI modelfrom lti.py and provides the wrapper
+    with the pytorch standard dataset function to provide data for neural nets.
+    It takes the number of simulation steps and the variance for the gaussian control signal
+    then simulates the data sequence. __getitem__ returns the ith index of the time series
+    """
+
+    def __init__(self, lti: LTISystem, num_steps: int, variance: float):
+        self.lti: LTISystem = lti
+        self.num_steps: int = num_steps
+        self.variance: torch.FloatTensor = torch.FloatTensor([variance])
+        self.U: torch.FloatTensor = (
+            torch.randn(num_steps, lti.B.shape[1]) * self.variance.sqrt()
+        )
+        self.t, self.y, self.x = lti.simulate(U=self.U.numpy(), dt=0.1)
+
+    def __len__(self):
+        return self.num_steps
+
+    def __getitem__(self, idx):
+        return self.y[idx]
